78cf06706a166db029d7925e5438df92102762ab
shevnindenis <62008920+shevnindenis@users.noreply.github.com>
2022-01-12 15:54:47 +0300

    Update Test_git.py

--- Test_git.py
+++ Test_git.py
@@ -1,15 +1,11 @@
 import time
 start_time = time.time()
 import random
-
 matrix = [random.randint(-10, 10) for i in range(20)]
-
-
 print('Matrix:', matrix)
 len_res = len(matrix)
 print('Matrix size:', len_res)
 sum = 0
-
 for i in range(len_res):
     for j in range(i+1,len_res):
        for k in range(j + 1,len_res):
