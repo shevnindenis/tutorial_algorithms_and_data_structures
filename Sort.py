320d59ea82a210592404f8c3bc182379c5620f2e
shevnindenis <shevnindi@gmail.com>
2022-01-25 17:20:25 +0300

    Add Quick Sort

--- Sort.py
+++ Sort.py
@@ -95,3 +95,32 @@ start_time6 = time.time()
 result_matrix = msort(matrix)
 #print(result_matrix)
 print("Merge_Sort --- %s seconds ---" % (time.time() - start_time6))
+
+
+#Quick_Sort
+matrix = [random.randint(-100000, 100000) for i in range(size_matrix)]
+#print(matrix)
+len_res = len(matrix)
+print(len_res)
+def sort(array=[12,4,5,6,7,3,1,15]):
+    """Sort the array by using quicksort."""
+
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        # Don't forget to return something!
+        return sort(less)+equal+sort(greater)  # Just use the + operator to join lists
+    # Note that you want equal ^^^^^ not pivot
+    else:  # You need to handle the part at the end of the recursion - when you only have one element in your array, just return the array.
+        return array
+print(array)
